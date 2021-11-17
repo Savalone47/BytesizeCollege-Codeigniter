2e74e60c57dcd5730e78505fbb3b6357ca22fe82
savalone47 <15yn450@esisalama.org>
2021-11-17 14:00:51 +0200

    update detail notification

--- application/views/notification/notification_detail.php
+++ application/views/notification/notification_detail.php
@@ -1,4 +1,41 @@
-<!-- start page content -->
+<?php if($this->session->type_user == 2):?>
+    <div class="page-content-wrapper">
+        <div class="page-content">
+            <div class="page-bar">
+                <div class="page-title-breadcrumb">
+                    <div class=" pull-left">
+                        <div class="page-title">Notifications</div>
+                    </div>
+                    <ol class="breadcrumb page-breadcrumb pull-right">
+                        <li>
+                            <a class="parent-item"
+                               href="<?php echo site_url('notification/index');?>">All Notifications</a>&nbsp;<i class="fa fa-angle-right"></i>
+                        </li>
+                        <li class="active">Notifications</li>
+                    </ol>
+                </div>
+            </div>
+            <?php foreach ($notification as $n): ?>
+                <div class="row">
+                    <div class="card">
+                        <div class="notification-ui_dd-content">
+                            <div class="notification-list notification-list--unread">
+                                <div class="notification-list_content">
+                                    <div class="notification-list_detail">
+                                        <p><b class="bold-muted underline"><?php echo $n['title']?></b></p>
+                                        <p><small><?php echo $n['author'];?></small></p>
+                                        <p class="text-muted"><?php echo $n['notification']?></p>
+                                        <p class="text-muted"><small><i class="fa  fa-calendar"></i>&nbsp;&nbsp; <?php echo $n['created_at']?></small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach;?>
+        </div>
+    </div>
+<?php elseif($this->session->type_user == 1):?>
     <div class="page-content-wrapper">
         <div class="page-content">
             <div class="page-bar">
@@ -19,30 +56,28 @@
                 </div>
             </div>
             <?php foreach ($notification as $n): ?>
-            <div class="row">
-                <div class="card">
-                    <div class="notification-ui_dd-content">
-                        <div class="notification-list notification-list--unread">
-                            <div class="notification-list_content">
-                                <div class="notification-list_detail">
-                                    <p><b class="bold-muted underline"><?php echo $n['title']?></b></p>
-                                    <p><small><?php echo $n['author'];?></small></p>
-                                    <p class="text-muted"><?php echo $n['notification']?></p>
-                                    <p class="text-muted"><small><i class="fa  fa-calendar"></i>&nbsp;&nbsp; <?php echo $n['created_at']?></small></p>
+                <div class="row">
+                    <div class="card">
+                        <div class="notification-ui_dd-content">
+                            <div class="notification-list notification-list--unread">
+                                <div class="notification-list_content">
+                                    <div class="notification-list_detail">
+                                        <p><b class="bold-muted underline"><?php echo $n['title']?></b></p>
+                                        <p><small><?php echo $n['author'];?></small></p>
+                                        <p class="text-muted"><?php echo $n['notification']?></p>
+                                        <p class="text-muted"><small><i class="fa  fa-calendar"></i>&nbsp;&nbsp; <?php echo $n['created_at']?></small></p>
+                                    </div>
                                 </div>
                             </div>
+                            <a href="<?php echo site_url('notification/edit/'.$n['id']);?>"
+                               class="btn btn-circle blue-bgcolor btn-sm">Edit</a>
+                            <a href="<?php echo site_url('notification/remove/'.$n['id']);?>"
+                               class="btn btn-circle btn-danger btn-sm"><i class="fa fa-trash-o "></i>Delete</a>
                         </div>
-
-                        <a href="<?php echo site_url('notification/edit/'.$n['id']);?>"
-                                class="btn btn-circle blue-bgcolor btn-sm">Edit</a>
-
-                       <a href="<?php echo site_url('notification/remove/'.$n['id']);?>"
-                           class="btn btn-circle btn-danger btn-sm"><i class="fa fa-trash-o "></i>Delete</a>
-
                     </div>
                 </div>
-            </div>
             <?php endforeach;?>
         </div>
     </div>
-    <!-- end page content -->
+<?php endif;?>
+<!-- end page content -->
